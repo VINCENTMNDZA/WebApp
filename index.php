e8dc6adcf0786aafb14ec3096d7078f4b0451c9b
VINCENTMNDZA <100121748+VINCENTMNDZA@users.noreply.github.com>
2022-02-21 17:34:36 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>My Home Page</title>
+    </head>
+  <body>
+    <h1>Welcome to My Website! I am Jill Anne L. Orquiales<3</h1>
+    <ul>
+      <li><a href="index.php">HOME</a></li> 
+      <li><a href="LT2/Index.html">Learning Task</a></li>
+    </ul>
+  </body>
+</html>aaaaaaaa
