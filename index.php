bb6c153b4cf4ea1bfa120daea9c58c9a6c2ba100
VINCENTMNDZA <100121748+VINCENTMNDZA@users.noreply.github.com>
2022-02-21 17:38:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <h1>Welcome to My Website! I am Jill Anne L. Orquiales<3</h1>
     <ul>
       <li><a href="index.php">HOME</a></li> 
-      <li><a href="LT2/Index.html">Learning Task</a></li>
+      <li><a href="Beefinbeer.html">Learning Task</a></li>
     </ul>
   </body>
 </html>
