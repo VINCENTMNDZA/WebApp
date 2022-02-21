9828992ccc1966ed11786d95b21a945e99fd43b5
VINCENTMNDZA <100121748+VINCENTMNDZA@users.noreply.github.com>
2022-02-21 19:01:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <h1>Welcome to My Website! John Vincent Mendoza</h1>
     <ul>
       <li><a href="index.php">HOME</a></li> 
-      <li><a href="index.html">Learning Task</a></li>
+      <li><a href="recipe.html">Learning Task</a></li>
     </ul>
   </body>
 </html>
