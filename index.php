57646e02d15544975d0fe2010f65ff06b6a2727d
VINCENTMNDZA <100121748+VINCENTMNDZA@users.noreply.github.com>
2022-04-04 22:46:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
     <head>  <title>MY HOME PAGE</title> </head>
     <body> <h1> WELCOME TO MY WEBSITE! I AM VINCENT MENDOZA</h1>
         <ul>
-            <li><a href="index.php>HOME</a></li>
-                            <li><a href="index.php>HOME</a></li>
-                        <li><a href="index.php>HOME</a></li>
-                                        <li><a href="index.php>HOME</a></li>
-                        <li><a href="index.php>HOME</a></li>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="recipe.html">Learning task 2</a></li>
+                            <li><a href="/LearningTask3/Learningtask33.html">Learning task 3</a></li>
+                                        <li><a href="INDEX.HTML">CLEMT1</a></li>
+                        <li><a href="">Learning Task 4</a></li>
                             </ul>
                             </body>
 
